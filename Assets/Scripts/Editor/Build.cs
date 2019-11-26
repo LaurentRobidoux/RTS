2c738d3ed1c07d02a1e5c546850e3d838c742380
Laurent Robidoux <laurent.robidoux.cem@gmail.com>
2019-11-26 15:27:00 -0500

    comment

--- Assets/Scripts/Editor/Build.cs
+++ Assets/Scripts/Editor/Build.cs
@@ -8,6 +8,7 @@ public class Build
     [MenuItem("MyMenu/Do Something")]
     private static void PerformBuild()
     {
+        //a comment
         string[] scenes = { "Assets/Scenes/SampleScene.unity" };
         BuildPipeline.BuildPlayer(scenes, "Build/app.exe", BuildTarget.StandaloneWindows64, BuildOptions.Development);
     }
